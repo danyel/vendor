bf98d5910cc3412244c8f002444698a91874906d
dnoulet <daniel.noulet@gmail.com>
2025-12-05 18:35:36 +0100

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+// project setup is done here ..
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		if _, err := w.Write([]byte("Hello World")); err != nil {
+
+		}
+	})
+
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
+}
